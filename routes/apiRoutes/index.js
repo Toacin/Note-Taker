bfc957d7122fcc3223aa2fdfe6f9a5be4b510086
Toacin <toacinp@outlook.com>
2022-10-14 19:29:31 -0700

    api routes have been modified to FS read and write to correct unexpected behavior

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,25 +2,30 @@ const router = require("express").Router();
 const fs = require("fs");
 
 router.get('/notes', (req,res) => {
-    const db = require("../../db/db.json");
-    res.json(db);
+    fs.readFile("db/db.json", (err, data) => {
+        const db = JSON.parse(data);
+        res.json(db);
+    })
 })
 
 router.post('/notes', (req,res) => {
-    const db = require("../../db/db.json");
     req.body.id = Math.floor(Math.random()*4000);
-    db.push(req.body);
-    let data = JSON.stringify(db, null, 2);
-    fs.writeFile("db/db.json", data, err => err ? console.log(err) : console.log("success"));
+    fs.readFile("db/db.json", (err, data) => {
+        const db = JSON.parse(data);
+        db.push(req.body);
+        fs.writeFile("db/db.json", JSON.stringify(db, null, 2), err => err ? console.log(err) : console.log("success"));
+    })
     res.json(req.body);
 })
 
 router.delete('/notes/:id', (req,res) => {
     const noteID = parseInt(req.params.id);
-    const UpToDatedb = require("../../db/db.json");
-    const result = UpToDatedb.filter(note => note.id !== noteID);
-    fs.writeFile("db/db.json", JSON.stringify(result, null, 2), err => err ? console.log(err) : console.log("success"));
-    res.json('success');
+    fs.readFile("db/db.json", (err, data) => {
+        const db = JSON.parse(data);
+        const updatedDB = db.filter((element) => element.id !== noteID);
+        fs.writeFile("db/db.json", JSON.stringify(updatedDB, null, 2), err => err ? console.log(err) : console.log("success"));
+    })
+    res.json("Success");
 })
 
 module.exports = router;
